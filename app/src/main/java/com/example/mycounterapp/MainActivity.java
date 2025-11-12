b27fedfef85383950871ef9fd8eb2c0a5c0678db
GTYang <gtq.yang@gmail.com>
2025-11-12 16:30:13 +0800

    commit

--- app/src/main/java/com/example/mycounterapp/MainActivity.java
+++ app/src/main/java/com/example/mycounterapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mycounterapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView welcom_text, counter_text;
+    Button btn;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        welcom_text = findViewById(R.id.welcomeText);
+        counter_text = findViewById(R.id.counterText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter_text.setText("" + increaseCounter());
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public int increaseCounter() {
+        System.out.println("Now counter:" + counter);
+        return ++counter;
+    }
+
 }
